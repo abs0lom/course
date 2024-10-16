e6b6456292fa233b0257ab459cf47c6bf96e902e
Absolom <olivier@make-it.fr>
2024-10-16 16:01:42 +0200

    feat: add state and callbatk

--- my-app/app/MyPage.tsx
+++ my-app/app/MyPage.tsx
@@ -1,7 +1,62 @@
-import { Text } from "react-native";
+import { useState, useCallback } from "react";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 
 function MyPage() {
-  return <Text>Hello World!</Text>;
+  // const useStateReturn = useState(""); // retourne un tableau contenant deux items
+  // const word = useStateReturn[0]; // le 1er item est stocké dans "word"
+  // const setWord = useStateReturn[1]; // le 2ond item est stocké dans "setWord"
+  const [word, setWord] = useState("initial word");
+
+  // function myFunction {
+  //   setWord("initial word");
+  // };
+  // const resetWord = useCallback(myFunction, []);
+
+  // équivalent à :
+
+  // const myFunction = () => {
+  //   setWord("initial word");
+  // };
+  // const resetWord = useCallback(myFunction, []);
+
+  // équivalent à :
+
+  const resetWord = useCallback(() => {
+    setWord("initial word");
+  }, []);
+
+  return (
+    <View style={styles.drawer}>
+      <Text style={styles.title}>Hello World!</Text>
+      <Button title="Reset word" onPress={resetWord} />
+      <Text style={styles.paragraph}>
+        Value stored in « word » : « {word} »
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a word"
+        onChangeText={setWord}
+        value={word}
+      ></TextInput>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  drawer: {
+    borderColor: "red",
+    borderWidth: 1,
+  },
+  title: {
+    color: "red",
+    textAlign: "center",
+    marginTop: 450,
+  },
+  paragraph: {},
+  input: {
+    borderColor: "blue",
+    borderWidth: 1,
+  },
+});
 
 export default MyPage;
