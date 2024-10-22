36d907e70075f8c170c8e558e209b9787b0c62e1
Absolom <olivier@make-it.fr>
2024-10-22 17:31:24 +0200

    feat(game): add clue functionnality

---
+++ memento/casting.js
@@ -0,0 +1,9 @@
+"chaine de caractère"
+
+"123" // chaine de caractère
+
+123 // nombre
+
+Number("123") === 123 // nombre
+
+String(123) === "123" // chaine de caractère
--- my-app/app/Game.tsx
+++ my-app/app/Game.tsx
@@ -1,29 +1,59 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 import { useCallback, useState } from "react";
 
 function Game() {
   const [numberToFind, setNumberToFind] = useState<any>(null);
+  const [numberToTest, setNumberToTest] = useState("");
+  const [clue, setClue] = useState("");
+
+  console.log("rerendered");
 
   const chooseAleaNumber = useCallback(() => {
     setNumberToFind(1);
   }, []);
 
+  const testAnswer = useCallback(() => {
+    if (Number(numberToTest) === numberToFind) {
+      setClue("C'est gagné !");
+    } else if (Number(numberToTest) < numberToFind) {
+      setClue("C'est plus !");
+    } else {
+      setClue("C'est moins !");
+    }
+  }, [numberToTest, numberToFind]);
+
   return (
     <View style={styles.drawer}>
       <Text style={styles.title}>Plus ou moins&nbsp;?</Text>
+      <Text>{clue}</Text>
       {numberToFind === null ? (
         <Button
           title="Essaie de deviner le nombre"
           onPress={chooseAleaNumber}
         />
       ) : (
-        "autre chose"
+        <>
+          <Text>Essaie de deviner le nombre</Text>
+          <TextInput
+            keyboardType="numeric"
+            style={styles.input}
+            value={numberToTest}
+            onChangeText={setNumberToTest}
+          />
+          <Button title="Envoyer" onPress={testAnswer} />
+        </>
       )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    borderColor: "black",
+    borderWidth: 2,
+    margin: 5,
+    padding: 5,
+  },
   drawer: {
     height: "100%",
     width: "100%",
